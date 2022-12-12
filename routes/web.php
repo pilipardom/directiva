811180f2b78f3df4a998c6583a0a7cdecf32203f
pilipardom <pilarp195@gmailcom>
2022-12-12 00:59:18 -0300

    feat: ruta de login

--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,13 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/saludo', function () {
     return view('app');
 });
 
-Route::get('users/{id}', function ($id) {
-    
+Route::get('/login', function () {
+    return view('login');
 });
